5ec31dfd6981f51756dbfb5eeab9ef5ac0014087
Gladys Mwangi <gladysmwangi04@yahoo.com>
2022-05-13 12:04:57 +0300

    updated the registration and login forms

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,2 +1,9 @@
-from flask import render_template
+from flask import render_template,redirect,url_for
+from .forms import *
+
+#views
+@auth.route('/')
+@auth.route()
+def register():
+    return render_template('signUp.html', form= form)
 
